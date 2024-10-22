535b99d70fe189bbcd8bd04087142a8c1396defe
thelaw51 <joegillard3@gmail.com>
2024-10-22 20:45:08 +0100

    got datagrid working sort of

---
+++ SpotifyShuffleProject.Client/Models/Song.cs
@@ -0,0 +1,11 @@
+﻿namespace SpotifyShuffleProject.Client.Models
+{
+    public class Song
+    {
+        public required string Name { get; set; }
+        public required string Artist { get; set; }
+        public required string Album { get; set; }
+        public  string? Image { get; set; }
+        public  string? Preview { get; set; }
+    }
+}
