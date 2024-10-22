32f9b1ffff2eae05caeb7c51bdb35616fce6d821
thelaw51 <joegillard3@gmail.com>
2024-10-22 20:45:08 +0100

    got datagrid working sort of

--- SpotifyShuffleProject.Client/Program.cs
+++ SpotifyShuffleProject.Client/Program.cs
@@ -4,6 +4,7 @@ using MudBlazor.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
